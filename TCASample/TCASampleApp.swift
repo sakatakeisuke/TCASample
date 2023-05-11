aed5be4d1581a7d226628ed16a4f39a3f48e6cdd
sakatakeisuke <ksmcd0725we0573@gmail.com>
2023-05-12 01:00:07 +0900

    projectsetup

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by 坂田圭佑 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
